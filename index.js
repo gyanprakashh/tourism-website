f81e91007e408fec4cb01e723a38ab74bc2a842d
Gyan Prakash <gyanprakash.gp98010@gmail.com>
2020-08-18 19:03:19 +0530

    major project

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const {authorize}=require('./servers/midddlewares/auth');
-const authRoute=require('./servers/routes/auth');
-const blogPost=require('./servers/routes/blog');
-const weatherTemp=require('./servers/routes/temp');
+const { authorize } = require("./servers/midddlewares/auth");
+const authRoute = require("./servers/routes/auth");
+const blogPost = require("./servers/routes/blog");
+const weatherTemp = require("./servers/routes/temp");
 const { NODE_PORT, NODE_ENV, DATABASE_URL } = process.env;
 const PORT = NODE_PORT || 8000;
 const isdevelopement = NODE_ENV === "developement";
@@ -31,19 +31,22 @@ app.use(function (req, res, next) {
   );
   next();
 });
-app.options('*', cors()) 
-app.use('/api',authRoute);
-app.use('/api/blog',authorize, blogPost);
-app.use('/api/weather',weatherTemp)
-mongoose.connect(DATABASE_URL,{
-    useCreateIndex:true,
-    useUnifiedTopology:true,
-    useFindAndModify:true,
-    useNewUrlParser:true
-}).then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`server is running on ${process.env.NODE_PORT}`)
-    })
-}).catch(err=>{
-    console.log(`Db connection faied ${err}`)
-});
+app.options("*", cors());
+app.use("/api", authRoute);
+app.use("/api/blog", authorize, blogPost);
+app.use("/api/weather", weatherTemp);
+mongoose
+  .connect(DATABASE_URL, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running on ${process.env.NODE_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Db connection faied ${err}`);
+  });
